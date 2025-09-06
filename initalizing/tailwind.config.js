1b8ea21ba50db5a0930adb7904427ddfb1a161c7
Shashank Soni <shashanksoni815@gmail.com>
2025-09-06 23:57:46 +0530

    data

---
+++ initalizing/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // adjust if your code is in a different folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
